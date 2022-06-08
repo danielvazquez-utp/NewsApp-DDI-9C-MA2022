25cb3bb164a25c1ed5973c9fa61eb305fb10d5f1
Daniel Vázquez <daniel.vazquez@utpuebla.edu.mx>
2022-06-08 10:55:13 -0500

    Métodos en MainActivity agregados

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -7,10 +7,17 @@ import android.widget.Button;
 import android.widget.TextView;
 import java.util.List;
 
+import com.example.news.client.RetrofitClient;
 import com.example.news.databinding.ActivityMainBinding;
+import com.example.news.entity.Message;
+import com.example.news.service.RetrofitApiService;
 
 import java.util.Arrays;
 import java.util.Random;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends Activity {
 
@@ -23,17 +30,24 @@ public class MainActivity extends Activity {
             "¡Listo para la acción!",
             "¡Al infinito y mas allá!"
     );
+    private RetrofitApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
         nTextView = findViewById(R.id.newsTextView);
         nButton = findViewById(R.id.moreButton);
+        */
+
         /**
         String mensaje = mensajes.get(4);
         nTextView.setText(mensaje);
          */
+
+        /**
         nButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +55,35 @@ public class MainActivity extends Activity {
                 // String mensaje = mensajes.get(4);
                 String mensaje = mensajes.get(aleatorio.nextInt(mensajes.size()));
                 nTextView.setText(mensaje);
+            }
+        });
+         */
+
+        initView();
+        initValues();
+        getMessage(49);
+    }
+
+    private void initView(){
+        nTextView = findViewById(R.id.newsTextView);
+        nButton = findViewById(R.id.moreButton);
+    }
+
+    private void initValues(){
+        apiService = RetrofitClient.getApiService();
+    }
+
+    private void getMessage(int id){
+        apiService.getMessageId(id).enqueue(new Callback<Message>() {
+            @Override
+            public void onResponse(Call<Message> call, Response<Message> response) {
+                Message message = response.body();
+                nTextView.setText(message.toString());
+            }
+
+            @Override
+            public void onFailure(Call<Message> call, Throwable t) {
+                nTextView.setText(t.toString());
             }
         });
     }
