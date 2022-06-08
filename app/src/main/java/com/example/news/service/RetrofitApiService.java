8c1721da4a159c3ae2a562ec1d8b828d8137ee7c
Daniel Vázquez <daniel.vazquez@utpuebla.edu.mx>
2022-06-08 10:11:04 -0500

    Definición del cuerpo de la interface RetrofitApiService

--- app/src/main/java/com/example/news/service/RetrofitApiService.java
+++ app/src/main/java/com/example/news/service/RetrofitApiService.java
@@ -1,4 +1,12 @@
 package com.example.news.service;
 
+import com.example.news.entity.Message;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface RetrofitApiService {
+    @GET("posts/{id}")
+    Call<Message> getMessageId(@Path("id") int id);
 }
