c4affb43703420a51dc3f2f018f6693c5acf5a25
Daniel Vázquez <daniel.vazquez@utpuebla.edu.mx>
2022-06-08 09:53:44 -0500

    Definición del cuerpo de la clase RetrofitClient

--- app/src/main/java/com/example/news/client/RetrofitClient.java
+++ app/src/main/java/com/example/news/client/RetrofitClient.java
@@ -1,4 +1,21 @@
 package com.example.news.client;
 
+import com.example.news.service.RetrofitApiService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+    public static final String URL_BASE = "https://jsonplaceholder.typicode.com/";
+    private static Retrofit retrofit;
+
+    public static RetrofitApiService getApiService(){
+        if(retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(URL_BASE)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(RetrofitApiService.class);
+    }
 }
